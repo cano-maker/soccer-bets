6b7de2d210dea014dbb99d158a896174dd8bd07c
Fabio Cano <fabiocano894@gmail.com>
2022-05-25 18:49:33 -0500

    add first tests for tournament controller

--- src/test/java/com/bets/soccer/SoccerApplicationTests.java
+++ src/test/java/com/bets/soccer/SoccerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SoccerApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
